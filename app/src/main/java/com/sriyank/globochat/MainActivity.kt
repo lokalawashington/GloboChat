46fa143335418ab7580ab3114ad221e98eb9775d
Washington <lokalawashington2019@gmail.com>
2023-01-16 13:40:41 +0300

    Worked on preference objects

--- app/src/main/java/com/sriyank/globochat/MainActivity.kt
+++ app/src/main/java/com/sriyank/globochat/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.sriyank.globochat
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.preference.PreferenceManager
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener{
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -27,9 +31,31 @@ class MainActivity : AppCompatActivity() {
 //
 //        // Link ActionBar with NavController
 //        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val autoReplyTime = sharedPreferences?.getString(getString(R.string.key_auto_reply_time), "")
+        Log.i("MainActivity", "Auto Reply Time: $autoReplyTime")
+    }
+
+    // called after the preference value has changed
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+        if(p1 == "key_status"){
+            val newStatus = p0?.getString(p1,"")
+            Toast.makeText(this,"New Status: $newStatus", Toast.LENGTH_LONG).show()
+        }
     }
 
 //    override fun onSupportNavigateUp(): Boolean {
 //        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 //    }
+    override fun onResume() {
+        super.onResume()
+    PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
+    }
 }
