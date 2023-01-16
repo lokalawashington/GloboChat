ec0388db289b6055eb1f975424a0964ba3fa2f6b
Washington <lokalawashington2019@gmail.com>
2023-01-16 11:49:46 +0300

    PreferenceScreen and dependency added

--- app/src/main/java/com/sriyank/globochat/MainActivity.kt
+++ app/src/main/java/com/sriyank/globochat/MainActivity.kt
@@ -18,18 +18,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get NavHost and NavController
-        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-        navController = navHostFrag.navController
-
-        // Get AppBarConfiguration
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-
-        // Link ActionBar with NavController
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        // Get NavHost and NavController
+//        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+//        navController = navHostFrag.navController
+//
+//        // Get AppBarConfiguration
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//
+//        // Link ActionBar with NavController
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
 }
