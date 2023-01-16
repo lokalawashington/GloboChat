fd60c92389597c7b793ed2889133909de4a2c3bf
Washington <lokalawashington2019@gmail.com>
2023-01-16 12:44:09 +0300

    opening a fragment when a preference is clicked using navigation component

--- app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
+++ app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 
@@ -13,5 +14,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings,rootKey)
+
+     val accSettingPref =  findPreference<Preference>("key_account_settings")
+        accSettingPref?.setOnPreferenceClickListener{
+
+            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+            val navController = navHostFragment.navController
+            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
+            navController.navigate(action)
+
+            true
+        }
     }
 }
